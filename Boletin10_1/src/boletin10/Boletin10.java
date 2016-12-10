8c4f83c5f030f52a5faf10b941ac9222258af39a
Adry <Adry@Sanisidro>
2016-12-10 22:03:33 +0100

    Juego solitario hecho,falta restablecer el numero aleatorio

---
+++ Boletin10_1/src/boletin10/Boletin10.java
@@ -0,0 +1,13 @@
+package boletin10;
+
+/**
+ *
+ * @author Adry
+ */
+public class Boletin10 {
+
+    public static void main(String[] args) {
+        JuegoAdivinanza juego1=new JuegoAdivinanza();
+        juego1.JuegoSolitario();
+    }
+}
---
+++ Boletin10_1/src/boletin10/JuegoAdivinanza.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin10;
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Adry
+ */
+public class JuegoAdivinanza {
+    int i=1,numero,nIntentos,resultado;
+    public void JuegoCoop(){
+        do{
+            numero=Integer.parseInt(JOptionPane.showInputDialog("J1 introduce un numero entre 1 y 50"));
+            nIntentos=Integer.parseInt(JOptionPane.showInputDialog("J1 introduce numero de intentos,minimo 1"));
+    }while ((numero<1||numero>50)||nIntentos<1);
+        do{
+            resultado=Integer.parseInt(JOptionPane.showInputDialog("J2 introduce resultado entre 1 y 50"));
+            if (resultado>numero)
+                JOptionPane.showMessageDialog(null,"El numero es menor");
+            else if (resultado<numero)
+                JOptionPane.showMessageDialog(null,"El numero es mayor");
+            else JOptionPane.showMessageDialog(null,"¡Acertaste!");
+            i++;
+        }while (resultado!=numero&&i<=nIntentos);
+    }
+    public void JuegoSolitario(){
+        Random rdm=new Random();
+       resultado=rdm.nextInt(49)+1;
+     do{
+         numero=Integer.parseInt(JOptionPane.showInputDialog("Introduce numero"));
+         if ((numero-resultado)==0&&(resultado-numero)==0)
+             JOptionPane.showMessageDialog(null,"Has acertado");
+         else if ((numero-resultado)<=5&&(resultado-numero)<=5)
+             JOptionPane.showMessageDialog(null,"Estas muy cerca");
+         else if ((numero-resultado)<10&&(resultado-numero)<10)
+             JOptionPane.showMessageDialog(null,"Estas cerca");
+         else if ((numero-resultado)<=20&&(resultado-numero)<=20)
+             JOptionPane.showMessageDialog(null,"Estas lejos");
+         else if ((numero-resultado)>20&&(resultado-numero)>20)
+             JOptionPane.showMessageDialog(null,"Estas muy lejos");
+         if (numero==0)
+             JOptionPane.showMessageDialog(null,"Juego cancelado");
+     }while (numero!=0);
+        System.out.println(resultado);
+    }
+}
